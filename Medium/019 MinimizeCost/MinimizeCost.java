a0760429b343d8a6b539d82b7dce470315931c68
Chang Ju Kim <kimc13@rpi.edu>
2020-06-19 15:41:42 -0400

    Added thoughts and test cases. Will be implementing Brandon's method

--- Medium/019 MinimizeCost/MinimizeCost.java
+++ Medium/019 MinimizeCost/MinimizeCost.java
@@ -1,3 +1,108 @@
+import java.util.Random;
+
 public class MinimizeCost {
+
+
+    public static int[][] randomCosts(int N, int K) {
+	Random rand = new Random();
+	int[][] costs = new int[K][N];
+	for (int i = 0; i < costs.length; i++) {
+	    costs[i] = new int[N];
+	    for (int j = 0; j < costs[i].length; j++) {
+		costs[i][j] = rand.nextInt(900) + 100;
+	    }
+	}
+	return costs;
+    }
+
+    public static String printArray(int[][] ary) {
+	String result = "[\n";
+	for (int i = 0; i < ary.length; i++) {
+	    result += "[";
+	    for (int j = 0; j < ary[i].length; j++) {
+		if (j == ary[i].length - 1) {
+		    result += ary[i][j] + "]\n";
+		} else {
+		    result += ary[i][j] + ", ";
+		}
+	    }
+	}
+	return result + "]";
+    }
+
+    public static int getDigit(int num) {
+	int digit = 1;
+	while (num > 9) {
+	    num = num / 10;
+	    digit += 1;
+	}
+	return digit;
+    }
+
+    // Ensures there is at least <space> space between each comma
+    public static String printArray(int[][] ary, int space) {
+	String result = "[\n";
+	for (int i = 0; i < ary.length; i++) {
+	    result += "[";
+	    for (int j = 0; j < ary[i].length; j++) {
+		String number = "" + ary[i][j];
+		int needSpaces = space - getDigit(ary[i][j]);
+		for (int z = 0; z < needSpaces; z++) {
+		    number += " ";
+		}
+		if (j == ary[i].length - 1) {
+		    result += number + "]\n";
+		} else {
+		    result += number + ", ";
+		}
+	    }
+	}
+	return result + "]";
+    }
+
+    // Replaces all but the lowest -> third lowest cost values with 0
+    public static int[][] truncateExcess(int[][] ary) {
+	System.out.println(ary.length);
+	if (ary.length <= 3) {
+	    return ary;
+	}
+	for (int j = 0; j < ary[0].length; j++) {
+	    int min = -3; // holds i index
+	    int min2 = -2;
+	    int min3 = -1;
+	    for (int i = 0; i < ary.length; i++) {
+		if (min < 0 || ary[i][j] < ary[min][j]) {
+		    if (min3 >= 0) {
+			ary[min3][j] = 0;
+		    }
+		    min3 = min2;
+		    min2 = min;
+		    min = i;
+		} else if (min2 < 0 || ary[i][j] < ary[min2][j]) {
+		    if (min3 >= 0) {
+			ary[min3][j] = 0;
+		    }
+		    min3 = min2;
+		    min2 = i;
+		} else if (min3 < 0 || ary[i][j] < ary[min3][j]) {
+		    if (min3 >= 0) {
+			ary[min3][j] = 0;
+		    }
+		    min3 = i;
+		} else if (ary[i][j] > ary[min3][j]) {
+		    ary[i][j] = 0;
+		} else {
+		    System.out.println("This case exists??");
+		}
+	    }
+	}
+	return ary;
+    }
+
     
+    public static void main(String[] args) {
+	int[][] ary = randomCosts(10, 4);
+	System.out.println("Original:\n" + printArray(ary, 3) + "\n\n");
+	System.out.println("Truncated:\n" + printArray(truncateExcess(ary), 3));
+    }
 }
--- Medium/019 MinimizeCost/README.md
+++ Medium/019 MinimizeCost/README.md
@@ -8,8 +8,33 @@ Given an N by K matrix where the nth row and kth column represents the cost to b
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-Approach 1:
+Approach 1 (depth first):
   1. From left to right, pick lowest cost values.
   2. If there is a conflict, branch. One will pick lowest for i and 2nd lowest for i+1. The other will pick lowest for i+1, 2nd lowest for i.
-  3. If there are further conflicts (picked 2nd lowest for k, k-1 or k+1 also conflicts), branch again with 3rd lowest.
+  3. If there are further conflicts (picked 2nd lowest for k, but k-1 or k+1 also conflicts), branch again with 3rd lowest.
   NOTE: Even if the changes propogate, you will always be choosing from 1st, 2nd, or 3rd lowest
+
+  Cost:
+    Best case (no conflicts): O(N)
+    Worst case (insane number of conflicts): O(N^3) [all lowest cost are same color, same with 2nd and 3rd]
+
+
+Approach 2 (butterfly effect):
+  1. From left to right, pick lowest cost values. 2nd lowest if there are conflicts. Calculate
+  2. Make first element the 2nd lowest. Make changes by flipping until there's no need. Calculate relative changes to cost from 1.
+  3. Make 2nd element 2nd lowest.
+  4. Make 2nd element 3rd lowest.
+  5. Keep going and so on and so forth
+  NOTE: There are ways to optimize which ones you're changing and seeing effects
+   	It should be very optimizable
+
+
+  Cost:
+    Best case (no conflicts): O(N)
+    Worst case (insane number of conflicts): O(N^3) i think
+
+
+Approach 3 (creds to Brandon for coming up with it):
+  Idea: Update costs as you go through the houses
+  1. House 1 costs are the same
+  2. House<i><color> cost = min(House<i-1> costs) that don't use same color 
