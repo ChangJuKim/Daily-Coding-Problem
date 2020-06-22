1d888eb4496b4aa3b394c632cb1784e1730e9f66
Chang Ju Kim <kimc13@rpi.edu>
2020-06-22 15:25:39 -0400

    Added comments

--- Medium/026 kthLastElement/KthLast.java
+++ Medium/026 kthLastElement/KthLast.java
@@ -7,14 +7,17 @@ public class KthLast {
 	ListIterator<Integer> forward_itr = list.listIterator();
 	ListIterator<Integer> behind_itr = list.listIterator();
 	int counter = 0;
+	// Moves the forward_itr so that it is k ahead of the behind one
 	while (forward_itr.hasNext() && counter < k) {
 	    forward_itr.next();
 	    counter += 1;
 	}
+	// Ensures that k isn't out of bounds
 	if (counter != k || k == 0) {
 	    System.out.println("k is not within the size of the list!");
-	    return -1;
+	    return -1; // Wasn't sure what to put. Options include throwing an exception or returning null
 	}
+	// Moves the iterators till forward reaches the end
 	while (forward_itr.hasNext()) {
 	    forward_itr.next();
 	    behind_itr.next();
