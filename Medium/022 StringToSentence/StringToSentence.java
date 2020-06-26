f33cace24f87501d34a777f53c01dc4dcf60e103
Chang Ju Kim <kimc13@rpi.edu>
2020-06-26 15:09:41 -0400

    Updated comments

--- Medium/022 StringToSentence/StringToSentence.java
+++ Medium/022 StringToSentence/StringToSentence.java
@@ -17,9 +17,11 @@ public class StringToSentence {
 	}
 	
 	// Find a word
-	// For loop is on message and not updateMessage because the index needs to match alreadyLookedThrough
+	// For loop parses through message and not updateMessage because the index needs to match alreadyLookedThrough
 	for (int endIndex = startIndex + 1; endIndex < message.length(); endIndex++) {
 	    String potentialWord = message.substring(startIndex, endIndex);
+	    // Tells other recursions that I'm looking at a word starting at endIndex
+	    // Note that if one recursion fails at finding a solution at index i, other recursions will also fail if they start there
 	    if (dictionary.contains(potentialWord)) {
 		if (alreadyLookedThrough[endIndex] == false) {
 		    alreadyLookedThrough[endIndex] = true;
@@ -41,10 +43,10 @@ public class StringToSentence {
     public static void main(String[] args) {
 	dictionary = new HashSet<String>();
 	dictionary.add("a");
+	dictionary.add("ab");
+	dictionary.add("cad");
 	dictionary.add("b");
-	dictionary.add("c");
-	dictionary.add("d");
-	String word = "abcbcabddbcabdcbe";
+	String word = "abcade";
 	alreadyLookedThrough = new boolean[word.length()];
 	ArrayList<String> result = convertToSentence(word, 0, new ArrayList<String>());
 	if (result == null) {
