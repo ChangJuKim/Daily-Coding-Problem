d3dcfb4b227c21f6bf50c73d09d65eb778ccae7a
Chang Ju Kim <kimc13@rpi.edu>
2020-06-30 19:44:13 -0400

    Finished 030!

--- Medium/030 RainwaterCollection/README.md
+++ Medium/030 RainwaterCollection/README.md
@@ -14,13 +14,31 @@ Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 Idea 1:
-    1. Start with two pointers, one from the left and one from the right, minLeft = minRight = 0
-    2. Until array[left] < minLeft, increment left. Same for right. Keep updating minLeft/minRight
+    1. Start with two pointers, one from the left and one from the right, maxLeft = maxRight = 0
+    2. Until array[left] < maxLeft, increment left. Same for right. Keep updating maxLeft/maxRight
       2.a. Idea is that you can ignore those walls because those areas will never collect water (it just runs off :<)
-      2.b. Once array[left] < minLeft and array[right] < minRight, the middle areas will collect water.
-    3. Start at minimum(minLeft, minRight). (for purposes of explanation will assume left < right)
-    4. i = left. While(array[i] <= minLeft)
-           total += minLeft - array[i]
-	   array[i] = minLeft
-    5. If array[i] > minLeft, minLeft = array[i]. Repeat 3-5 until left and right meet
-    
+      2.b. Once array[left] < maxLeft and array[right] < maxRight, the middle areas will collect water.
+    3. Start at minimum(maxLeft, maxRight). (for purposes of explanation will assume left < right)
+    4. i = left. While(array[i] <= maxLeft)
+           total += maxLeft - array[i]
+	   array[i] = maxLeft
+    5. If array[i] > maxLeft, maxLeft = array[i]. Repeat 3-5 until left and right meet
+
+
+Idea 1 SIMPLIFIED:
+    1. Start with two pointers. leftWall = ary[0]. rightWall = ary[-1]
+    2. Increment minimum(leftWall, rightWall).
+       2.a. If ary[i] < Wall, collect water.
+       2.b. If ary[i] > Wall, update Wall height
+    3. Keep going till pointers meet
+
+    Edit: Nevermind, you have to find the left and right walls first
+
+
+~~~~~~~~~~~~~~~~~~~~~~
+
+Done!
+Wasn't too bad, implemented idea 1, then decided to simplify it, then realized I needed
+the part that I removed
+
+Did some extensive testing
--- Medium/030 RainwaterCollection/RainwaterCollection.java
+++ Medium/030 RainwaterCollection/RainwaterCollection.java
@@ -1,3 +1,82 @@
 public class RainwaterCollection {
-    
+
+    /*
+      Edge cases:
+        1. Elevation never descends
+	2. Elevation never ascends
+	3. Elevation is just a mountain (like this: ^ )
+
+     */
+    public static int waterTrapped(int[] elevation) {
+	if (elevation.length <= 2) {
+	    return 0;
+	}
+	int leftIndex = 0;
+	int rightIndex = elevation.length - 1;
+	int leftWall = elevation[leftIndex];
+	int rightWall = elevation[rightIndex];
+	int trappedWater = 0;
+	// Find two walls
+	while (leftIndex <= rightIndex && leftWall <= elevation[leftIndex]) {
+	    leftWall = elevation[leftIndex];
+	    leftIndex += 1;
+	}
+	while (rightIndex >= leftIndex && rightWall <= elevation[rightIndex]) {
+	    rightWall = elevation[rightIndex];
+	    rightIndex -= 1;
+	}
+	
+	while (leftIndex <= rightIndex) {
+	    if (leftWall <= rightWall) {
+		if (elevation[leftIndex] < leftWall) {
+		    System.out.println("Left: " + trappedWater + " + " + (leftWall - elevation[leftIndex]) + " = " + (trappedWater + leftWall - elevation[leftIndex]));
+		    trappedWater += leftWall - elevation[leftIndex];
+		} else {
+		    leftWall = elevation[leftIndex];
+		}
+		leftIndex += 1;
+	    } else {
+		if (elevation[rightIndex] < rightWall) {
+		    System.out.println("Right: " + trappedWater + " + " + (rightWall - elevation[rightIndex]) + " = " + (trappedWater + rightWall - elevation[rightIndex]));
+		    trappedWater += rightWall - elevation[rightIndex];
+		} else {
+		    rightWall = elevation[rightIndex];
+		}
+		rightIndex -= 1;
+	    }
+	}
+	return trappedWater;
+    }
+
+    public static void printArray(int[] ary) {
+	System.out.print("[");
+	for (int i = 0; i < ary.length; i++) {
+	    if (i == ary.length - 1) {
+		System.out.print("" + ary[i]);
+	    } else {
+		System.out.print("" + ary[i] + ", ");
+	    }
+	}
+	System.out.println("]");
+    }
+
+
+    public static void test(int[] ary) {
+	printArray(ary);
+	System.out.println("water trapped: " + waterTrapped(ary) + "\n");
+    }
+
+    public static void main(String[] args) {
+	test(new int[] {2, 1, 2});
+	test(new int[] {3, 0, 1, 3, 0, 5});
+	test(new int[] {0, 1, 3, 3, 4, 5});
+	test(new int[] {5, 4, 3, 3, 1, 0});
+	test(new int[] {1, 2, 3, 2, 1});
+	test(new int[] {1, 2, 3, 3, 2, 1});
+	test(new int[] {});
+	test(new int[] {1});
+	test(new int[] {1, 2});
+	test(new int[] {2, 1});
+	test(new int[] {1, 2, 3, 10, 3, 11, 5, 13, 6, 3, 3, 10, 3, 2, 1});
+    }
 }
